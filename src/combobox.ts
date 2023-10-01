237221dd2837f9f9b4f486bec96b88ebbaa8fa7c
Rafael Kassner <rafael@kassner.com.br>
2023-10-01 18:44:52 +0200

    handle selected click and removal

--- src/combobox.ts
+++ src/combobox.ts
@@ -6,10 +6,10 @@ export default class Combobox {
     selected: HTMLElement;
     comboboxController: GithubCombobox;
 
-    constructor(inputSelector : string, listSelector : string, selectedSelector : string) {
-        this.input = document.querySelector(inputSelector);
-        this.list = document.querySelector(listSelector);
-        this.selected = document.querySelector(selectedSelector);
+    constructor(inputSelector: string, listSelector: string, selectedSelector: string) {
+        this.input = document.querySelector(inputSelector) as HTMLInputElement;
+        this.list = document.querySelector(listSelector) as HTMLElement;
+        this.selected = document.querySelector(selectedSelector) as HTMLElement;
 
         this.comboboxController = new GithubCombobox(this.input, this.list);
         this.bindEvents();
@@ -23,28 +23,62 @@ export default class Combobox {
 
         this.input.addEventListener('focus', () => this.toggleList());
         this.input.addEventListener('blur', () => {
-            this.list.hidden = true;
-            this.comboboxController.clearSelection();
-            this.comboboxController.stop();
+            /**
+             * setTimeout added to mitigate upstream issue.
+             *
+             * @see https://github.com/github/combobox-nav/issues/54
+             */
+            setTimeout(() => {
+                this.list.setAttribute('hidden', 'hidden');
+                this.comboboxController.clearSelection();
+                this.comboboxController.stop();
+            }, 50);
         });
 
-        this.list.addEventListener('combobox-commit', (event : Event) => {
+        this.list.addEventListener('combobox-commit', (event: Event) => {
             const target = event.target as HTMLElement;
-            let content = target.textContent;
+            let content = target.textContent || '';
 
             if (target.attributes.getNamedItem('data-create')?.value == 'true') {
                 content = this.input.value;
             }
 
-            const template = this.selected.querySelector('template').cloneNode(true) as HTMLTemplateElement;
+            const template = this.selected.querySelector('template')?.cloneNode(true) as HTMLTemplateElement;
             const newHtml = template.innerHTML.replace('{{value}}', content);
-            this.selected.insertAdjacentHTML('beforeend', newHtml)
+            this.selected.insertAdjacentHTML('beforeend', newHtml.trim());
 
-            this.list.hidden = true;
+            this.list.setAttribute('hidden', 'hidden');
             this.input.value = '';
             this.comboboxController.clearSelection();
             this.comboboxController.stop();
-        })
+        });
+
+        /**
+         * Can be removed once upstream is fixed
+         * @see https://github.com/github/combobox-nav/issues/54
+         */
+        this.list.addEventListener('mousedown', (event) => {
+            const target = event.target as HTMLElement;
+            if (target.matches('[role=option]')) {
+                const ev = new CustomEvent('combobox-commit', { bubbles: true, detail: { event } });
+                target.dispatchEvent(ev);
+            }
+        });
+
+        this.list.addEventListener('mouseover', (event) => {
+            const target = event.target as HTMLElement;
+            if (target.matches('li')) {
+                this.list.querySelectorAll('li[aria-selected=true]').forEach((value: Element) => {
+                    value.removeAttribute('aria-selected');
+                });
+                target.setAttribute('aria-selected', 'true');
+            }
+        });
+
+        this.selected.addEventListener('click', (event) => {
+            const target = event.target as HTMLElement;
+            target.closest('[role=listitem]')?.remove();
+        });
     }
 
     filterList() {
@@ -55,14 +89,18 @@ export default class Combobox {
             }
 
             const el = item as HTMLElement;
-            item.setAttribute('hidden', el.innerText.toLowerCase().includes(this.input.value.toLowerCase()) ? 'hidden' : '');
+            if (el.innerText.toLowerCase().includes(this.input.value.toLowerCase())) {
+                item.removeAttribute('hidden');
+            } else {
+                item.setAttribute('hidden', 'hidden');
+            }
         }
 
         if (this.input.value.length == 0) {
             return;
         }
 
-        const template = this.list.querySelector('template[data-role=create]').cloneNode(true) as HTMLTemplateElement;
+        const template = this.list.querySelector('template[data-role=create]')?.cloneNode(true) as HTMLTemplateElement;
         const newHtml = template.innerHTML.replace('{{value}}', this.input.value);
         this.list.insertAdjacentHTML('beforeend', newHtml)
     }
@@ -71,11 +109,11 @@ export default class Combobox {
         const hidden = this.input.value.length === 0;
 
         if (hidden) {
+            this.list.setAttribute('hidden', 'hidden');
             this.comboboxController.stop();
         } else {
+            this.list.removeAttribute('hidden');
             this.comboboxController.start();
         }
-
-        this.list.hidden = hidden;
     }
 }
